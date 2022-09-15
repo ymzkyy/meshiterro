2fafe2df6f491af4be50ca199a957d2add55bc56
ymzkyy <110673799+ymzkyy@users.noreply.github.com>
2022-09-15 10:40:46 +0000

    16章

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -21,7 +21,7 @@ class PostImagesController < ApplicationController
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy 
-    redirect_to '/post_images' 
+    redirect_to post_images_path 
   end
 
    private
